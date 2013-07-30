1eab3159973acf843468b9fdf221064b29033137
David Arnold <davida@pobox.com>
2013-07-30 23:07:24 +1000

    Basic packaging

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 
 setup(name="monjon",
-      version="0.1",
+      version="1.0b1",
       description="Network debugger",
       author="David Arnold",
       author_email="d@0x1.org",
       url="http://www.0x1.org/monjon",
-      packages=["monjon", "monjon.gui", "monjon.proxy", "monjon.robot"],
-      scripts=["bin/monjon", "bin/monjon-robot"],
+      packages=["monjon"],
+      scripts=["scripts/monjon-robot", "scripts/monjon-cli", "scripts/monjon-proxy"],
       )
 
 
